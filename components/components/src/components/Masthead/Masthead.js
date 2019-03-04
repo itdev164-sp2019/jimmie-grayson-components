759d96f44685f09992b3b4b41d22088c0f680dc9
jimmie grayson <graysojr@gmatc.matc.edu>
2019-03-04 06:23:32 -0600

    deleting files

--- components/components/src/components/Masthead/Masthead.js
+++ components/components/src/components/Masthead/Masthead.js
@@ -1,11 +1,11 @@
-import React from 'react' 8.5k (gzipped: 3.4K)
-import PropTypes from 'prop-types' 1.6k (gzipped: 838)
-import { Box } from 'rebass' 15.3k (gzipped: 4.7k)
-import styled from 'styled-components' 43.4k (gzipped)
-import { Link }from 'rebass' 15.3k (gzipped: 4.7k)
+import React from 'react'
+import PropTypes from 'prop-types'
+import { Box } from 'rebass'
+import styled from 'styled-components'
+import { Link }from 'rebass'
 
 const Outer = styled(Box)`
-    height: ${props -> props.height};
+    height: ${props => props.height};
     width: 100%;
     background-color: rebeccapurple;
     margin-bottom: 1.45rem;
